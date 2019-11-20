36d6cd75104581c024d1d79b2bf414ca977f87f0
Chris Elliott <cjelliott@gmail.com>
2019-11-20 16:04:27 +1100

    modified user-detail component prior to ng update

--- UserManager.Web/ClientApp/src/app/components/user-detail/user-detail.component.ts
+++ UserManager.Web/ClientApp/src/app/components/user-detail/user-detail.component.ts
@@ -1,8 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { NgForm } from '@angular/forms';
+
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
+    private user: User;
+    @ViewChild('detailForm', null) detailForm: NgForm;
+
+    constructor(
+        private route: ActivatedRoute,
+        private userService: UserService,
+        private location: Location,
+    ) { }
+
+    ngOnInit() {
+        this.getUser();
+    }
+
+    getUser(): void {
+        const id = +this.route.snapshot.paramMap.get('id');
+        if (id == 0) {
+            this.user = new User();
+            this.user.id = 0;
+
+        } else {
+            this.userService.getUser(id).subscribe(user => this.user = user);
+        }
+    }
+
+    goBack(): void {
+        this.detailForm.reset();
+        this.location.back();
+    }
+
+    save(): void {
+        if (this.user.id == 0) {
+            this.userService.addUser(this.user)
+                .subscribe(() => this.goBack());
+        } else {
+            this.userService.updateUser(this.user)
+                .subscribe(() => this.goBack());
+        }
+    } 
 }
